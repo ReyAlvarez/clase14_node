edbb7b221bfedfbfe2ef1d95fb8cfaca62c845b8
Nacho Rey Alvarez <nacho.rei@gmail.com>
2023-10-18 12:19:57 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+/*
+? Git - Github 
+--> Comandos de Git
+    -> git init
+    -> git add .
+    -> git add  (ej: index.html)
+    -> git status
+    -> git commit  -m "Mensaje xx"
+    ?- git push -u origin master
+
+    ==> Buenas practicas con commits
+        - usar verbos que describan la accion: agregar, cambiar, remover
+        - corto y conciso
+        - agregar un prefijo (chore: para realizar tareas - fix: para arreglar bugs)
+            ex. git commit -m "chore: remove search form navBar"
+
+  *==> GitHub ==>         
+*/
